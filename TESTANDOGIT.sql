04876963fdc1f72cfea89509b4a05c8cd31d43fe
unknown <herlanrodriguez@gmail.com>
2025-04-15 21:30:21 -0300

    Comando de INSERT aluno

--- TESTANDOGIT.sql
+++ TESTANDOGIT.sql
@@ -1,4 +1,13 @@
 
 -- algumacoisa
 
-CREATE DATABASE ESCOLA
+CREATE DATABASE ESCOLA;
+
+CREATE TABLE alunos (
+    id INT NOT NULL PRIMARY KEY
+    nome VARCHAR (255) NOT NULL
+    email VARCHAR (255)
+);
+
+
+INSERT INTO ALUNOS (id, nome, email) VALUE (1,"Herlan Rodriguez","herlanrodriguez009@gmail.com");
